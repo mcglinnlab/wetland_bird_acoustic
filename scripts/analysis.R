b201ed632a82efd50229c5be357e101ed3e7d891
Ansley Williamson <ansleywilliamson@gmail.com>
2023-04-06 08:25:15 -0400

    rename script files to make more sense

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -1,63 +1,165 @@
-qqnorm(sr)
-qqline(sr,col=2)
+library(vegan)
+library(readr)
+library(dplyr)
+library(easyCODA)
 
-#Try to transform the data with sqrt
-sr_sqrt <- sqrt(sr)
-qqnorm(sr_sqrt)
-qqline(sr_sqrt,col=2)
-hist(sr_sqrt)
-plotNormalHistogram(sr_sqrt, xlab="sr_sqrt", main = "Histogram of sr_sqrt")
+comm$CHNA
 
-#Transform data w cube root
-sr_cube<- sign(sr) * abs(sr)^(1/3)
-qqnorm(sr_cube)
-qqline(sr_cube,col=2)
-hist(sr_cube)
-plotNormalHistogram(sr_cube,xlab="sr_cuve", main = "Histogram of sr_cube")
+#Chuck-will's widow - NIGHTIME
+write.csv(row.names(comm[comm$CHNA > 1,]), file = './data/CHNA_detections.csv')
 
-#Transform data w log
-srlog<- log(sr)
-qqnorm(srlog)
-qqline(srlog,col=2)
-plotNormalHistogram(srlog, xlab="srlog", main = "Histogram of srlog")
+# distribution of species richness
+hist(rowSums(comm > 0), xlab= "# of Observations within 5 min interval",
+     main = "Distribution of Species Richness")
 
-ks.test(sr,y='pnorm',alternative = "two.sided")
-#Asymptotic one-sample Kolmogorov-Smirnov test
-#data:  sr
-#D = 0.84134, p-value < 2.2e-16
+#subsample out in equal amounts of upland and wetlands or statistically model for
+#the temporal and spatial autocorrelation. pseudo-replication.
 
-kruskal.test(hab,Common.name,data=dat)
-#Kruskal-Wallis rank sum test
-#data:  hab and Common.name
-#Kruskal-Wallis chi-squared = 743.49, df = 116, p-value < 2.2e-16
+# look at species ranks based upon number of occurrences
+sort(colSums(comm > 0), dec = T)
 
-diversity(sr, dat, equalize.groups = TRUE)
+row.names(comm)
+habitat <- ifelse(substring(comm$wetland_id, 1, 1) == 'U', 'upland', 'wetland')
+#need to assign Halidon vs stono if we care to compare
+#property <- ifelse(substring(site_id, 1, 1) == 'H', 'Halidon', 'Stono')
 
-# random sampling with function sample
-random1<-comm[sample(1:nrow(comm), size = 300 ), ]
-rsr<-rowSums(random1>0)
-rhab<- sample(1:nrow(comm), size = 300)
-boxplot(rsr~rhab)
+table(habitat)
 
-bird_rda <- rda(comm ~ habitat)
-anova(bird_rda)
-PLOT.RDA(bird_rda, map = "asymmetric")
+sr <- rowSums(comm > 0)
+plot(density(sr))
+sr_site_id <- tapply(sr, site_id, mean)
+srlog <- log(sr)
 
-t.test(srlog~habitat)
+boxplot(srlog ~ habitat, ylab = "log(Species Richness)", xlab="Habitat", 
+        main="Species Richness by Habitat")
+boxplot(sr~site_id, ylab = "Species Richness", xlab="Site ID", 
+        main="Species Richness per Site")
+boxplot(sr~property_wet, ylab = "Species Richness", xlab="Wetland Sites by Property",
+        main="Wetland SR by Property")
 
-summary(aov(srlog~habitat))
-#F-value is 65.686 and p-value is 0.001, so we can reject the null hypothesis.
-#species richness and shannon index. Throw away some data so that sample sizes match. 
-#could try resampling approach. Resample randomly to create same size data.
+hist(sr, xlab="Species Richness", main = "Histogram of Species Richness")
 
-#filter soundwave files so only sounds above certain decimal are kept? 
-#install.packages("warbleR")
-#install.packages("Rraven")
-#does it match species composition? Bring in jackson's data and compare the community
-#matrix. Level of diversity: species ID & compositional diff btwn the two. 
-#Merge into single comm matrix. With sample column acoustic vs human.
-#convert data into 5 min intervals. Add veg data and area/perimeters.
-#Canopy cover variables showed signal in jackson's results. Distance from nearest
-#tree and wet area.
+#separate comm matrix by habitat type
+library(dplyr)
+srhab <- cbind(sr,habitat)
+srhab <- as.data.frame(srhab)
+wetsr <- srhab %>% filter(habitat=='wetland', na.rm=TRUE)
+upsr <- srhab %>% filter(habitat=='upland', na.rm=TRUE)
 
-#sr avg to occurence stat
+dat_pc <- read.csv('https://raw.githubusercontent.com/mcglinnlab/wetland_birds/main/data/filtered_data/clean_bird_dat.csv')
+#note: in dat_pc from Jackson that wetland_id is equivalent to what I've called
+# site_id; therefore let's rename wetland_id to site_id for simplicity
+#jackson's data is subset from 0y-25 m rather than previous total which was 50m.
+head(dat_pc)
+comm_pc <- dat_pc[ , c('wetland_id', names(dat_pc)[12:66])]
+comm_pc[1:5, 1:5]
+sr_pc <- rowSums(comm_pc[ , -(1)] > 0)
+plot(density(sr_pc))
+
+sr_avg <- tapply(sr_pc, comm_pc$wetland, mean)
+sr_avg
+plot(sr_avg)
+
+#transform into data frame
+comm_ptct <- as.data.frame(comm_pc)
+
+sr_avg_df <- data.frame(wetland_id = names(sr_avg), S = sr_avg)
+sr_site_id_df <- data.frame(wetland_id = names(sr_site_id), S = sr_site_id)
+
+sr_merge <- merge(sr_avg_df, sr_site_id_df, by = 'wetland_id', all = TRUE)
+
+plot(S.y ~ S.x, data = sr_merge)
+abline(a = 0 , b=1)
+
+#compress point counts and acoustic into a single value per sample, tapply or aggregate
+#merge & get 2 NOCA's- one for PC & one for acoustic
+comm_ptct <- aggregate(comm_ptct, by = list(comm_ptct$wetland_id), function(x) sum(x > 0) / length(x))
+# drop wetland id and rename group 1 to wetland id
+names(comm_ptct)
+comm_ptct = subset(comm_ptct, select = -wetland_id)
+names(comm_ptct)= c("wetland_id", names(comm_ptct) [-1])
+names(comm_ptct)
+## wetland id pivots wrong.
+
+#pivot data frame to long format
+library(tidyr)
+comm <- pivot_longer(comm, cols = !wetland_id)
+comm_ptct <- pivot_longer(comm_ptct, cols = !wetland_id)
+
+#add column to dataframes indicating methods
+comm_ptct$method <- "ptct"
+comm$method <- "acoustic"
+
+#row append community matrices
+comm_both <- rbind(comm, comm_ptct)
+
+# drop sites not found in both sampling methods 
+acoustic_sites <- unique(comm_both$wetland_id[comm_both$method == 'acoustic'])
+sites_with_both <- acoustic_sites[acoustic_sites %in% unique(comm_both$wetland_id[comm_both$method == 'ptct'])]
+sites_with_both
+
+comm_both <- subset(comm_both, wetland_id %in% sites_with_both)
+
+sp_occ <- pivot_wider(comm_both, names_from = 'method', values_from = value)
+sp_occ$acoustic <- ifelse(is.na(sp_occ$acoustic), 0, sp_occ$acoustic)
+sp_occ$ptct <- ifelse(is.na(sp_occ$ptct), 0, sp_occ$ptct)
+
+
+plot(acoustic ~ ptct, data = sp_occ, type = 'n')
+with(sp_occ, text(ptct, acoustic, labels = name, cex = 0.5))
+abline(a = 0 , b= 1)
+
+sp_occ_agg <- sp_occ %>% 
+    group_by(name) %>% 
+    summarize(acoustic = mean(acoustic), ptct = mean(ptct))
+
+
+plot(acoustic ~ ptct, data = sp_occ_agg, type = 'n')
+with(sp_occ_agg, text(ptct, acoustic, labels = name, cex = 0.5))
+abline(a = 0 , b= 1)
+
+S_acou <- with(sp_occ, tapply(acoustic, list(wetland_id), sum))
+S_ptct <- with(sp_occ, tapply(ptct, list(wetland_id), sum))
+
+V_acou <- with(sp_occ, tapply(acoustic, list(wetland_id), 
+                              function(x) sum(x * (1 - x))))
+V_ptct <- with(sp_occ, tapply(ptct, list(wetland_id),
+                              function(x) sum(x * (1 - x))))
+
+par(mfrow=c(1,2))
+plot(S_ptct, S_acou)
+abline(a = 0, b=1)
+plot(V_ptct, V_acou)
+abline(a = 0, b=1)
+
+sr_both <- rowSums(comm_both > 0)
+sr_both_avg <- tapply(sr_both, comm_both$wetland, mean)
+length(sr_both)
+length(comm_both)
+plot(sr_both_avg)
+
+boxplot(comm_both$value~ comm_both$method)
+
+upland <- comm_both$wetland_id
+boxplot(comm_both$value ~ comm_both$wetland_id)
+
+comm_both$habitat <- ifelse(substring(comm_both$wetland_id, 1, 1) == 'U', 'upland', 'wetland')
+
+boxplot(comm_both$value ~ comm_both$habitat)
+
+#next: Dig into details & see what, why, where. Test upland vs wetlands to see diff
+#between habitat type - open vs veg, vary depending on ptct radius or conf level.
+#study scale shows that methods are slightly redundant but for most part they are
+#detecting diff community comp. 
+#(Should we remove all species but songbirds? One more layer of processing & see if 
+#that corrects the axes).
+#which species did Jackson miss that I picked up? Go back & validate by listening.
+
+#NEED A PLOT THAT SHOWS DIFF BETWEEN ACOUSTIC SR & POINT COUNT SR
+# diversity accumulation; point counts miss rare things that come through (rare tail)
+# separate analysis pathway. 
+
+#build occupancy matrix for both mine and jackson's species
+#proceed as if we dropped the wonky guys 
+
+#examine the redundancy vs complementary info in acoustic methods.
---
+++ scripts/prelim_analysis.R
@@ -0,0 +1,63 @@
+qqnorm(sr)
+qqline(sr,col=2)
+
+#Try to transform the data with sqrt
+sr_sqrt <- sqrt(sr)
+qqnorm(sr_sqrt)
+qqline(sr_sqrt,col=2)
+hist(sr_sqrt)
+plotNormalHistogram(sr_sqrt, xlab="sr_sqrt", main = "Histogram of sr_sqrt")
+
+#Transform data w cube root
+sr_cube<- sign(sr) * abs(sr)^(1/3)
+qqnorm(sr_cube)
+qqline(sr_cube,col=2)
+hist(sr_cube)
+plotNormalHistogram(sr_cube,xlab="sr_cuve", main = "Histogram of sr_cube")
+
+#Transform data w log
+srlog<- log(sr)
+qqnorm(srlog)
+qqline(srlog,col=2)
+plotNormalHistogram(srlog, xlab="srlog", main = "Histogram of srlog")
+
+ks.test(sr,y='pnorm',alternative = "two.sided")
+#Asymptotic one-sample Kolmogorov-Smirnov test
+#data:  sr
+#D = 0.84134, p-value < 2.2e-16
+
+kruskal.test(hab,Common.name,data=dat)
+#Kruskal-Wallis rank sum test
+#data:  hab and Common.name
+#Kruskal-Wallis chi-squared = 743.49, df = 116, p-value < 2.2e-16
+
+diversity(sr, dat, equalize.groups = TRUE)
+
+# random sampling with function sample
+random1<-comm[sample(1:nrow(comm), size = 300 ), ]
+rsr<-rowSums(random1>0)
+rhab<- sample(1:nrow(comm), size = 300)
+boxplot(rsr~rhab)
+
+bird_rda <- rda(comm ~ habitat)
+anova(bird_rda)
+PLOT.RDA(bird_rda, map = "asymmetric")
+
+t.test(srlog~habitat)
+
+summary(aov(srlog~habitat))
+#F-value is 65.686 and p-value is 0.001, so we can reject the null hypothesis.
+#species richness and shannon index. Throw away some data so that sample sizes match. 
+#could try resampling approach. Resample randomly to create same size data.
+
+#filter soundwave files so only sounds above certain decimal are kept? 
+#install.packages("warbleR")
+#install.packages("Rraven")
+#does it match species composition? Bring in jackson's data and compare the community
+#matrix. Level of diversity: species ID & compositional diff btwn the two. 
+#Merge into single comm matrix. With sample column acoustic vs human.
+#convert data into 5 min intervals. Add veg data and area/perimeters.
+#Canopy cover variables showed signal in jackson's results. Distance from nearest
+#tree and wet area.
+
+#sr avg to occurence stat
