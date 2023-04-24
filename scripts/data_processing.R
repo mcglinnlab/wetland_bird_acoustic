a7225f1dcaedcaf5dc442fc85efc59480c6c64eb
Ansley Williamson <ansleywilliamson@gmail.com>
2023-04-24 12:22:25 -0400

    update interval code placement

--- scripts/data_processing.R
+++ scripts/data_processing.R
@@ -22,6 +22,7 @@ hab <- ifelse(grepl('U', dat$site_id), 'upland', 'wetland')
 dat <- cbind(dat, hab)
 
 acou_splist <- unique(dat$Scientific.name)
+
 #import BBL species codes & merge
 bbl_sp_codes <- read.csv("data/IBP-AOS-LIST22.csv")
 all(acou_splist %in% bbl_sp_codes$SCINAME)
@@ -41,6 +42,13 @@ dat_sub$date_time <- as.POSIXlt(dat_sub$date_time, tz = 'EST',
 dat_sub$start_time <-  as.POSIXlt(dat_sub$start_time, tz = 'EST',
                                   format = "%m/%d/%Y %H:%M")
 
+# now adjust start times so that they are in 5 min intervals
+for (i in 1:nrow(dat_sub)) {
+    if ((dat_sub$start_time[i]  - dat_sub$date_time[i]) > 4) {
+        dat_sub$date_time[i] <- dat_sub$date_time[i] + 5*60
+    }
+}
+
 # fix times of a few sites where clock was wrong
 # HH01 needs 4 hours subtracted
 dat_sub$start_time[dat_sub$site_id == "HH01"] <- dat_sub$start_time[dat_sub$site_id == "HH01"] - 4*60*60
@@ -55,6 +63,19 @@ dat_sub$start_time[dat_sub$site_id == "SP02"] <- dat_sub$start_time[dat_sub$site
 # SP09 needs 4 hours subtracted
 dat_sub$start_time[dat_sub$site_id == "SP09"] <- dat_sub$start_time[dat_sub$site_id == "SP09"] - 4*60*60
 
+#fix date time
+# HH01 needs 4 hours subtracted
+dat_sub$date_time[dat_sub$site_id == "HH01"] <- dat_sub$date_time[dat_sub$site_id == "HH01"] - 4*60*60
+# HH02 needs 4 hours subtracted
+dat_sub$date_time[dat_sub$site_id == "HH02"] <- dat_sub$date_time[dat_sub$site_id == "HH02"] - 4*60*60
+# HH04 needs 4 hours subtracted
+dat_sub$date_time[dat_sub$site_id == "HH04"] <- dat_sub$date_time[dat_sub$site_id == "HH04"] - 4*60*60
+# HH05 needs 4 hours subtracted
+dat_sub$date_time[dat_sub$site_id == "HH05"] <- dat_sub$date_time[dat_sub$site_id == "HH05"] - 4*60*60
+# SP02 needs 8 hours added!
+dat_sub$date_time[dat_sub$site_id == "SP02"] <- dat_sub$date_time[dat_sub$site_id == "SP02"] + 8*60*60
+# SP09 needs 4 hours subtracted
+dat_sub$date_time[dat_sub$site_id == "SP09"] <- dat_sub$date_time[dat_sub$site_id == "SP09"] - 4*60*60
 
 # sunup
 dawn_times <- as.POSIXlt(paste(dat_sub$date, "6:00"), tz = 'EST',
@@ -66,13 +87,6 @@ end_times <- as.POSIXlt(paste(dat_sub$date, "9:00"), tz = 'EST',
 # subset dat_sub to dawn chorus times 
 dat_sub <- dat_sub[dat_sub$start_time > dawn_times & dat_sub$start_time < end_times, ]
 
-# now adjust start times so that they are in 5 min intervals
-for (i in 1:nrow(dat_sub)) {
-    if ((dat_sub$start_time[i]  - dat_sub$date_time[i]) > 4) {
-        dat_sub$date_time[i] <- dat_sub$date_time[i] + 5*60
-    }
-}
-
 # make unique id that has site_id and date_time
 dat_sub$site_date <- with(dat_sub, paste(site_id, date_time, sep = '_'))
 comm <- with(dat_sub, tapply(Confidence, list(site_date, sp_code),
@@ -82,15 +96,19 @@ sum(comm)
 
 comm[, 1:5]
 
+#avg species richness by wetland_id
 wetland_id <- sapply(strsplit(row.names(comm), split = '_', fixed = TRUE), function(x) x[1])
+sumcomm <- aggregate(comm, by = list(wetland_id), function(x) sum(x))
 comm <- aggregate(comm, by = list(wetland_id), function(x) sum(x) / length(x))
-names(comm)
 #rename group 1 to wetland_id
 names(comm) = c("wetland_id", names(comm) [-1])
+names(comm)
 
 # convert to data.frame
 comm <- as.data.frame(comm)
+sumcomm <- as.data.frame(sumcomm)
 
 #export data for analysis
+write.csv(sumcomm, file = "./data/clean_data/sumcomm.csv", row.names = FALSE)
 write.csv(comm, file = "./data/clean_data/comm.csv", row.names = FALSE)
 write.csv(dat_sub, file = "./data/clean_data/subset_data.csv", row.names = FALSE)
